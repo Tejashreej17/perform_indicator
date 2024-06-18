9adce92a7d090b461d1ca1a0d3134951e7e9ec83
Tejashree <tejujoshi17@gmail.com>
2024-06-18 21:17:15 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name='studdent_perform_indicator',
+version='0.0.1',
+author='Tejashree',
+author_email='tejujoshi17@gmail.com',
+packages=find_packages(),
+
+)
